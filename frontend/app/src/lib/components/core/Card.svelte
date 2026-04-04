e0043b995701825646246b8db05023c0a92d1f11
Goldmite <aurijus.sus@gmail.com>
2026-04-04 20:35:52 +0300

    added theme styles and base layout, theme toggle

---
+++ frontend/app/src/lib/components/core/Card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div
+	class="rounded-tl-2xl rounded-tr-lg rounded-br-2xl rounded-bl-lg border border-main-border p-2"
+>
+	{@render children()}
+</div>
--- frontend/app/src/routes/+layout.svelte
+++ frontend/app/src/routes/+layout.svelte
@@ -1,9 +1,69 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	const headerTitle = 'Coffee Recommender System';
+	let isDarkTheme = $state(false);
+
+	onMount(() => {
+		isDarkTheme = localStorage.getItem('theme') === 'dark';
+		updateTheme();
+	});
+
+	function toggleTheme() {
+		isDarkTheme = !isDarkTheme;
+		localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+		updateTheme();
+	}
+
+	function updateTheme() {
+		document.documentElement.classList.toggle('dark', isDarkTheme);
+	}
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<script>
+		const theme = localStorage.getItem('theme');
+		if (theme === 'dark') {
+			document.documentElement.classList.addEventListener('dark');
+		}
+	</script>
+</svelte:head>
+
+<header
+	class="sticky top-0 flex h-16 w-full items-center justify-between bg-secondary p-4 text-white-taupe"
+>
+	<a href="/" class="text-xl font-bold uppercase"> {headerTitle} </a>
+	<div class="flex gap-4">
+		<button
+			type="button"
+			aria-label="Toggle dark theme"
+			class="rounded-full p-2"
+			onclick={toggleTheme}
+		>
+			{#if isDarkTheme}
+				<span class="bean icon-[streamline--coffee-bean] text-main-mid"></span>
+			{:else}
+				<span class="bean text-bean-roast icon-[streamline--coffee-bean-solid] text-main-mid"
+				></span>
+			{/if}
+		</button>
+	</div>
+</header>
+
+<div class="min-h-svh w-full p-4">
+	{@render children()}
+</div>
+
+<footer></footer>
+
+<style>
+	.bean {
+		width: var(--text-xl);
+		height: var(--text-xl);
+	}
+</style>
