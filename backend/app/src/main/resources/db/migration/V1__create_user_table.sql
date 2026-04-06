596428c22b1a2bb2fa8a5cf62ad6dd8e7e2d1c18
Goldmite <aurijus.sus@gmail.com>
2026-04-06 22:04:00 +0300

    Replaced postgres with pgvector postgres image and enabled it, adjusted user to have role since replacing db

--- backend/app/src/main/resources/db/migration/V1__create_user_table.sql
+++ backend/app/src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,7 +1,10 @@
+CREATE TYPE user_role AS ENUM ('admin', 'tester', 'user')
+
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    role user_role NOT NULL DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
