28b81f98af1cf3667370f53332f24546ce894e11
Goldmite <aurijus.sus@gmail.com>
2026-04-12 17:09:21 +0300

    added service tests and adjusted update request dto

--- backend/app/src/main/java/org/recsys/dto/shop/ShopUpdateRequest.java
+++ backend/app/src/main/java/org/recsys/dto/shop/ShopUpdateRequest.java
@@ -1,11 +1,30 @@
 package org.recsys.dto.shop;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-public class ShopUpdateRequest extends ShopRequest {
+@AllArgsConstructor
+public class ShopUpdateRequest {
 
     @NotNull
     private Integer id;
+
+    @NotBlank
+    @Length(max = 96)
+    private String name;
+
+    @NotBlank
+    @URL
+    private String shopUrl;
+
+    @JsonProperty("is_active")
+    private Boolean isActive;
 }
